afad3bc7fe2478d0bb267ddf955f1e0a07a1b588
Daniel Sisovic <d.sisovic777@gmail.com>
2023-12-02 23:13:53 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <>
+      <p>Test</p>
+    </>
+  )
+}
+
+export default App
